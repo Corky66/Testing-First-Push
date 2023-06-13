24b81173e391dff831e8d16f2ae04ec284b9bb86
Corky66 <cory.m.phillips66@gmail.com>
2023-06-12 21:17:59 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let season = "Autumn";
+
+switch(season){
+    case "Summer": console.log("Hot day outside");
+    break;
+    case  "Fall" || "Autumn": console.log("Might need a jacket");
+    break;
+    case "Winter": console.log("Bundle up");
+    break;
+    case "Spring": console.log("Check for rain");
+    break;
+};
